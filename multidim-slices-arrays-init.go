d5288baa03d27062acd7cee1f023d7575e35ef98
Fausto Salazar Mora <fausto.ds.mora@gmail.com>
2021-02-13 14:55:02 -0600

    Create multidim-slices-arrays-init.go

---
+++ multidim-slices-arrays-init.go
@@ -0,0 +1,17 @@
+package main
+
+// Multiarray & multislice initialization
+// See https://stackoverflow.com/questions/39804861/what-is-a-concise-way-to-create-a-2d-slice-in-go
+// http://www.golangbootcamp.com/book/collection_types
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, playground")
+	var slc = make([][]int, 5) // slices are descriptors
+	var arr [3][3]int. // arrays are values
+	fmt.Println(slc)
+	fmt.Println(arr)
+}
