ceaa07fc46fe2c28a4de5ba2a25b29fdb8cd3dce
Fausto Salazar <fsalazar@brightcove.com>
2021-03-07 12:42:13 -0600

    comparison of trees with channels async

--- binary-tree-channels/main.go
+++ binary-tree-channels/main.go
@@ -5,6 +5,28 @@ import (
 	"fmt"
 )
 
+func Same(t1, t2 *tree.Tree) bool {
+	c1 := make(chan int)
+	c2 := make(chan int)
+
+	go t1.Walk(c1)
+	go t2.Walk(c2)
+
+	for {
+		x1, ok1 := <-c1
+		x2, ok2 := <-c2
+		switch {
+		case ok1 != ok2:
+			return false
+		case !ok1:
+			return true
+		case x1 != x2:
+			return false
+		default:
+		}
+	}
+}
+
 func main() {
 	t := tree.New(1)
 
@@ -15,4 +37,6 @@ func main() {
 	for v := range c {
 		fmt.Printf(" %d", v)
 	}
+
+	fmt.Println(Same(tree.New(1), tree.New(1)))
 }
