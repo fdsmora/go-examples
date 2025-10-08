93a78ba6ee0d8d1002bb78bb6fdc05fef5ce8407
fausto <fausto.ds.mora@gmail.com>
2025-10-08 11:18:33 -0600

    improvements

--- data-structures-algorithms/patterns/slidingwindow/CollectMaxFruitsFromTrees.go
+++ data-structures-algorithms/patterns/slidingwindow/CollectMaxFruitsFromTrees.go
@@ -1,5 +1,7 @@
 package slidingwindow
 
+import "math"
+
 // leetcode problem 904
 // runtime O(2n) -> O(n), as e visits n fruits, and in the worst case, s goes back from e-1 to 1,
 // so when e = n-1, s visits almost all n elements backwards, adding another n so O(n + n)
@@ -10,29 +12,24 @@ func totalFruit(fruits []int) int {
 		return len(fruits)
 	}
 	var sum, max, s, e int
-	basket := make(map[int]int, basketSize)
+	basket := make(map[int]bool, basketSize)
 
 	for e = 0; e < len(fruits); e++ {
-		_, ok := basket[fruits[e]]
-		if len(basket) == 2 && !ok {
+		if len(basket) == 2 && !basket[fruits[e]] {
 			sum = e - s
-			if sum > max {
-				max = sum
-			}
-			basket = make(map[int]int, basketSize)
-			basket[fruits[e]] = 0
-			basket[fruits[e-1]] = 0
+			max = int(math.Max(float64(max), float64(sum)))
+			basket = make(map[int]bool, basketSize)
+			basket[fruits[e]] = true
+			basket[fruits[e-1]] = true
 			for s = e - 1; s >= 1 && fruits[s] == fruits[s-1]; s-- {
 			}
 		} else {
-			basket[fruits[e]] = 0
+			basket[fruits[e]] = true
 		}
 	}
 	sum = e - s
-	if sum > max {
-		max = sum
-	}
-	return max
+
+	return int(math.Max(float64(max), float64(sum)))
 }
 
 // 0  1  2  3  4  5
