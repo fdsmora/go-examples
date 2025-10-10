9e1e233f3c808bcbdc91c4c71a32928d72d46e77
fausto <fausto.ds.mora@gmail.com>
2025-10-10 13:48:34 -0600

    explicacion incluida

--- data-structures-algorithms/patterns/twopointers/ContainerWithMostWater.go
+++ data-structures-algorithms/patterns/twopointers/ContainerWithMostWater.go
@@ -4,6 +4,14 @@ import (
 	"math"
 )
 
+// Idea de por qué funciona mover el puntero de la menor columna. Llamemos a este puntero j.
+/* Hay que mover un puntero hacia adentro, pero esto reduce el ancho del contenedor.
+   La idea es encontrar una columna más alta para compensar ese ancho.
+   Al mover j, si encontramos una columna más alta que i y j, habrá una ganancia de volumen entre i y la nueva columna.
+   Si este nuevo volumen no es mayor que el anterior, moveremos entonces i y si la nueva columna es mayor, habrá una nueva
+   ganancia de volumen. Entonces, en general, al mover el puntero más pequeño (j), uno está descartando la pérdida de volumen de haber elegido en su lugar j,
+   ya que i provee la mayor ganancia al ser mayor.
+*/
 func MaxArea(height []int) int {
 	front, back := 0, len(height)-1
 	mostWater := 0
