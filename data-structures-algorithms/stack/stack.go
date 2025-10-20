178fd450dea0ede15a3340967d7fe133a703207d
fausto <fausto.ds.mora@gmail.com>
2025-10-20 15:15:40 -0600

    my stack implementation in go

---
+++ data-structures-algorithms/stack/stack.go
@@ -0,0 +1,33 @@
+package stack
+
+type Stack[T interface{}] []T // type Stack[T any] []T
+
+// O(1)
+func (s *Stack[T]) Peek() T {
+	if len(*s) == 0 {
+		var zero T
+		return zero
+	}
+	return (*s)[len(*s)-1]
+}
+
+// O(1) amortized
+func (s *Stack[T]) Pop() (T, bool) {
+	if len(*s) == 0 {
+		var zero T
+		return zero, false
+	}
+	idx := len(*s) - 1
+	val := (*s)[idx]
+	*s = (*s)[:idx]
+	return val, true
+}
+
+// O(1) amortized
+func (s *Stack[T]) Push(val T) {
+	*s = append(*s, val)
+}
+
+func (s *Stack[T]) Len() int {
+	return len(*s)
+}
