74f6751a91499379e39b25e209e582802538631a
fausto <fausto.ds.mora@gmail.com>
2026-01-19 13:11:24 -0600

    optimizations and commented previous code

--- data-structures-algorithms/patterns/backtracking/numberOfPaths.go
+++ data-structures-algorithms/patterns/backtracking/numberOfPaths.go
@@ -1,28 +1,63 @@
 package backtracking
 
+/*
+Number of Paths
+You’re testing a new driverless car that is located at the Southwest (bottom-left) corner of an n×n grid. The car is supposed to get to the opposite, Northeast (top-right), corner of the grid. Given n, the size of the grid’s axes, write a function numOfPathsToDest that returns the number of the possible paths the driverless car can take.
+
+the car may move only in the white squares
+
+For convenience, let’s represent every square in the grid as a pair (i,j). The first coordinate in the pair denotes the east-to-west axis, and the second coordinate denotes the south-to-north axis. The initial state of the car is (0,0), and the destination is (n-1,n-1).
+
+The car must abide by the following two rules: it cannot cross the diagonal border. In other words, in every step the position (i,j) needs to maintain i >= j. See the illustration above for n = 5. In every step, it may go one square North (up), or one square East (right), but not both. E.g. if the car is at (3,1), it may go to (3,2) or (4,1).
+
+Explain the correctness of your function, and analyze its time and space complexities.
+
+Example:
+
+input:  n = 4
+
+output: 5 # since there are five possibilities:
+          # “EEENNN”, “EENENN”, “ENEENN”, “ENENEN”, “EENNEN”,
+          # where the 'E' character stands for moving one step
+          # East, and the 'N' character stands for moving one step
+          # North (so, for instance, the path sequence “EEENNN”
+          # stands for the following steps that the car took:
+          # East, East, East, North, North, North)
+*/
+
 //import "fmt"
 
+// commented code is for the inefficient version that stores all paths as strings
 func NumOfPathsToDest(n int) int {
 	if n == 1 {
 		return 1
 	}
-	var res []string
-	res = visit(0, 0, n, "", res)
-	return len(res)
+	/*
+		 	var res []string
+			res = visit(0, 0, n, "", res)
+			return len(res)
+	*/
+	return visit(0, 0, n, 0, 0)
 }
 
-func visit(i, j, n int, path string, res []string) []string {
+// func visit(i, j, n int, path string, res []string) []string {
+func visit(i, j, n, pathLen, count int) int {
 	if i >= n || j >= n {
-		return res
+		//return res
+		return count
 	}
 	if i == n-1 && j == n-1 {
-		return append(res, path)
+		//return append(res, path)
+		return count + 1
 	}
 	if i+1 < n {
-		res = visit(i+1, j, n, path+"E", res)
+		//res = visit(i+1, j, n, path+"E", res)
+		count = visit(i+1, j, n, pathLen+1, count)
 	}
 	if j+1 < n && j+1 <= i {
-		res = visit(i, j+1, n, path+"N", res)
+		// res = visit(i, j+1, n, path+"N", res)
+		count = visit(i, j+1, n, pathLen+1, count)
 	}
-	return res
+	//return res
+	return count
 }
